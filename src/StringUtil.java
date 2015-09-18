14a597ae8379f55cf585ead3f0e0c831cb649dcd
aolin xu <xal2008sc@gmail.com>
2015-09-18 15:14:55 +0800

    字符串处理类

---
+++ src/StringUtil.java
@@ -0,0 +1,19 @@
+/**
+ * 处理字符串
+ * @author honoka
+ */
+public final class StringUtil {
+  //小写常量
+  public static final StringFormatConstants LOWER_CASE = StringFormatConstants.LOWER_CASE;
+
+  //大写常量
+  public static final StringFormatConstants UPPER_CASE = StringFormatConstants.UPPER_CASE;
+
+  //字符串处理常量枚举
+  private enum StringFormatConstants {
+	  //小写
+	  LOWER_CASE,
+	  //大写
+	  UPPER_CASE;
+  }
+}
